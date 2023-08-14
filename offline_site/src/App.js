f4f805b8b0824b18c422227a48b99d184f02c5b0
Arantuil <gideon.t.v.d.k@hotmail.com>
2023-08-14 17:19:15 +0200

    finished the offline version of the site

--- offline_site/src/App.js
+++ offline_site/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+          The website is temporarily offline, you can always interact with the dapp directly on the block explorer: <a 
+          className='contractLink'
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noreferrer"
+          href='https://explorer.energyweb.org/token/0x5A547Ad0cE7140110aE945F00b7D8dF6f58257d7/token-transfers'>EnergyWebSpark contract</a>
+        </p>
       </header>
     </div>
   );
