06ef1eee27d3f82a94b2c863a1c3602686156ec6
Arantuil <gideon.t.v.d.k@hotmail.com>
2023-08-18 04:06:00 +0200

    added offline page, and page open timer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 // pages
 import Home from './pages/Home';
@@ -13,12 +14,27 @@ import EditCampaign from './pages/EditCampaign';
 import CampaignDetails from './pages/CampaignDetails';
 import Profile from './pages/Profile';
 
+import Offline from './pages/Offline';
+
 // components
 import Navbar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [isOffline, setIsOffline] = useState(false);
+
+  const [currentTimestamp, setCurrentTimestamp] = useState(Math.floor(Date.now() / 1000));
+  const openTimestamp = 1692324200
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTimestamp(Math.floor(Date.now() / 1000))
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
+    isOffline === false && currentTimestamp > openTimestamp ? (
     <div className="xs:p-2 p-3 sm:p-6 md:p-8 relative bg-[#1C1D30] min-h-screen flex flex-row">
       <Provider store={store}>
         <Router>
@@ -34,6 +50,17 @@ function App() {
         </Router>
       </Provider>
     </div>
+    ) : (
+      <div className="xs:p-2 p-3 sm:p-6 md:p-8 relative bg-[#1C1D30] min-h-screen flex flex-row">
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Offline difference={openTimestamp-currentTimestamp} />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </div>
+    )
   );
 }
 
